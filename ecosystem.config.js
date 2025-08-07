af921052d37b31f2a3769724985338f99e5451cd
Kelvin Maina <thiongokelvin5@gmail.com>
2025-08-07 19:15:43 +0300

    Authenticate

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'websockets-app',
+    script: 'index.js',
+    cwd: '/var/www/myWebsockets',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: '/var/log/pm2/websockets-error.log',
+    out_file: '/var/log/pm2/websockets-out.log',
+    log_file: '/var/log/pm2/websockets-combined.log'
+  }]
+};
