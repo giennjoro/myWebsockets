8de2973d8d7eceb9edb71ed83f92fedcfae74235
giennjoro <georgenjoroge977@gmail.com>
2025-08-21 22:17:47 +0300

    Add UI and Authentication to the server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env: {
+    env_production: {
       NODE_ENV: 'production',
       PORT: 3001,
       DASHBOARD_USERNAME: 'writersAdmin',
